fde2032f8111706f0cdda5bc76f6f5f0a0cbedef
Mikel Sigler <mzsigler@gmail.com>
2022-05-11 19:52:22 -0500

    Made decent progress on add new book modal

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
 bookArray = [];
 
+const addNewButton = document.getElementById('btnAddNew');
+const closeModalButton = document.getElementById('close-modal');
+const buttonSubmit = document.getElementById('modal-submit-button');
+const titleInput = document.getElementById('title-field');
+const authorInput = document.getElementById('author-field');
+const pagesInput = document.getElementById('pages-field');
+const readInput = document.getElementById('read-field');
+
+buttonSubmit.addEventListener('click', handleForm);
+
+function handleForm() {
+    let tit = titleInput.value.toString();
+    let auth = authorInput.value.toString();
+    let pag = pagesInput.value;
+    let red = readInput.value.toString();
+
+
+    modalToggle();
+    clearFields();
+    
+    addNewBook(tit, auth, pag, red);
+
+    
+}
+
+function clearFields() {
+    titleInput.value = '';
+    authorInput.value = '';
+    pagesInput.value = '';
+    readInput.value = '';
+}
+
+
+addNewButton.addEventListener('click', modalToggle);
+closeModalButton.addEventListener('click', modalToggle);
+
+
+function modalToggle() {
+    const modBackground = document.getElementById('modal-wrapper');
+    const modal = document.getElementById('add-new-modal');
+
+    modBackground.classList.toggle('hide');
+    modal.classList.toggle('hide');
+}
 
 
 function Book(title, author, pages, read){
@@ -9,9 +53,11 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}. ${this.pages} pages. ${this.read}`;
-}
+
+
+// Book.prototype.info = function() {
+//     return `${this.title} by ${this.author}. ${this.pages} pages. ${this.read}`;
+// }
 
 
 addNewBook = function(title, author, pages, read) {
@@ -22,24 +68,22 @@ addNewBook = function(title, author, pages, read) {
 }
 
 
-
-
-
-
-
 addNewBook("The Stand", "Stephen King", 4982, "Read");
 addNewBook("Catch-22", "Joseph Heller", 412, "Read");
 addNewBook("Slaughterhouse Five", "Kurt Vonnegut", 391, "Read");
+addNewBook("Maximum Boost", "Corky Bell", 482, "Read");
+addNewBook("Norweigian Wood", "Haruki Murakami", 376, "Read")
 
-
-function displayBook(book) {
+function displayBook(book, index) {
     const wrapper = document.getElementById("wrapper");
     const newCard = document.createElement('div');
     newCard.classList.add('card');
+    newCard.classList.add(index);
     newCard.innerHTML = `<h2> Title: ${book.title} </h2>` +
                         `<p> Author: ${book.author} </p>` +
                         `<p> Pages: ${book.pages} </p>` +
-                        `<p> Read: ${book.read} </p>`;
+                        `<p> Read: ${book.read} </p>`
+                        ;
 
     
     wrapper.appendChild(newCard);
@@ -47,8 +91,6 @@ function displayBook(book) {
 
 
 
-bookArray.forEach(book => {
-    displayBook(book);
+bookArray.forEach((book, index) => {
+    displayBook(book, index)
 });
-
-console.table(bookArray);
